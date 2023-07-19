708e56ae9c3dd7364e6e06887baa4a5dca864369
Jean F. dos Santos <jeanddg@hotmail.com>
2023-07-19 18:21:46 -0300

    feat: usa modelSerializer

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -2,36 +2,18 @@ from rest_framework import serializers
 
 from agenda.models import Agendamento
 
+from django.utils import timezone
 
-class AgendamentoSerializer(serializers.Serializer):
-    data_horario = serializers.DateTimeField()
-    nome_cliente = serializers.CharField(max_length=100)
-    email_cliente = serializers.EmailField()
-    telefone_cliente = serializers.CharField(max_length=20)
 
-    def create(self, validated_data):
-        """
-        Create and returns a new  agendamento
-        """
-        return Agendamento.objects.create(**validated_data)
+class AgendamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agendamento
+        fields = ['id', "data_horario", "nome_cliente",
+                  "email_cliente", "telefone_cliente"]
 
-    def update(self, instance, validated_data):
-        """
-            updates and returns a agendamento
-        """
-        instance.data_horario = validated_data.get(
-            'data_horario', instance.data_horario
-        )
-        instance.nome_cliente = validated_data.get(
-            'nome_cliente', instance.nome_cliente
-        )
-        instance.email_cliente = validated_data.get(
-            'email_cliente', instance.email_cliente
-        )
-        instance.telefone_cliente = validated_data.get(
-            'telefone_cliente', instance.telefone_cliente
-        )
+    def validate_data_horario(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError(
+                'Agendamento não pode ser feito no passado!')
 
-        instance.save()
-
-        return instance
+        return value
