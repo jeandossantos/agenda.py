055815af3f3de1a1d6cb649aff861e2525274ade
Jean F. dos Santos <jeanddg@hotmail.com>
2023-07-22 19:37:11 -0300

    refactor: use mixins

--- agenda/views.py
+++ agenda/views.py
@@ -3,62 +3,50 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 
 from .models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 
 
-class AgendamentoDetail(APIView):
-    def get(self, request, id):
+class AgendamentoDetail(
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Agendamento.objects.all()
+    serializer_class = AgendamentoSerializer
+    lookup_field = "id"
+
+    def get(self, request, id, *args, **kwargs):
         """
         Get a schedule by ID
         """
-        obj = get_object_or_404(Agendamento, id=id)
-        serializer = AgendamentoSerializer(obj)
+        return self.retrieve(request, *args, **kwargs)
 
-        return Response(serializer.data)
-
-    def patch(self, request, id):
+    def patch(self, request, *args, **kwargs):
         """
         Update a schedule
         """
-        obj = get_object_or_404(Agendamento, id=id)
+        return self.partial_update(request, *args, **kwargs)
 
-        data = {
-            "nome_cliente": request.data.get('nome_cliente')
-        }
-
-        serializer = AgendamentoSerializer(
-            obj, data=data, partial=True
-        )
-
-        if serializer.is_valid():
-            serializer.save()
-
-            return Response(data=serializer.data)
-        else:
-            return Response(serializer.errors)
-
-    def delete(self, request, id):
-        obj = get_object_or_404(Agendamento, id=id)
-        obj.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        """Delete a schedule"""
+        return self.destroy(request, *args, **kwargs)
 
 
-class AgendamentoList(APIView):
-    def get(self, request):
-        agendamentos = Agendamento.objects.all()
-        serializer = AgendamentoSerializer(agendamentos, many=True)
+class AgendamentoList(
+        mixins.ListModelMixin,
+        mixins.CreateModelMixin,
+        generics.GenericAPIView
+):
+    queryset = Agendamento.objects.all()
+    serializer_class = AgendamentoSerializer
 
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request):
-        serializer = AgendamentoSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-
-            return Response(status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
