9781f63547127d8c7e2ad95f5b9607c6e0cae6e8
Jean F. dos Santos <jeanddg@hotmail.com>
2023-07-26 18:45:05 -0300

    feat: get horarios disponiveis

---
+++ agenda/utils.py
@@ -0,0 +1,28 @@
+from typing import Iterable
+from agenda.libs.brasil_api import is_feriado
+
+from datetime import date, datetime, timedelta, timezone
+
+from agenda.models import Agendamento
+
+
+def get_horarios_disponiveis(data: date) -> Iterable[datetime]:
+    """Retornas os horários disponíveis do dia informado, caso o dia seja um feriado"""
+
+    if is_feriado(data):
+        return []
+
+    start = datetime(year=data.year, month=data.month,
+                     day=data.day, hour=9, minute=0, tzinfo=timezone.utc)
+    end = datetime(year=data.year, month=data.month,
+                   day=data.day, hour=18, minute=0, tzinfo=timezone.utc)
+
+    delta = timedelta(minutes=30)
+    horarios_disponiveis = set()
+
+    while (start < end):
+        if not Agendamento.objects.filter(data_horario=start).exists():
+            horarios_disponiveis.add(start)
+        start = start + delta
+
+    return horarios_disponiveis
